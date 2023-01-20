6a8a40360854bc89cd761e84e77f99c86262b88a
Vladislav Isayev <vladislav.isaev@vk.team>
2023-01-20 23:06:24 +0300

    Core command

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sadasd
+
+        asdasd
+
+        sad
+        onProvideAssistData(onProvideAssistData(
+
+        ))
     }
 }
